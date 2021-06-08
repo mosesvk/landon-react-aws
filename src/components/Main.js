2de9d512592ad733618b6db6239416cdfa4b69ed
Moses Kaumatule <kaumatule93@gmail.com>
2021-06-07 18:33:37 -0600

    update new components for the main js file

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Welcome from './Welcome'
+import InfoHotel from './InfoHotel'
+import Footer from './Footer'
+
+const Main = () => {
+  return (
+    <main id="wrapper">
+      <Welcome />
+
+      <InfoHotel />
+
+      <Footer />
+    </main>
+  )
+}
+
+export default Main
